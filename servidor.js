45707a21fe9ad9d416811184194daaa6e10e0aa5
ktinho456 <ktinho456@users.noreply.github.com>
2015-06-10 17:44:19 -0300

    teste.js
    seila

--- servidor.js
+++ servidor.js
@@ -1,4 +1,7 @@
-  var io = require('socket.io').listen(8080);
+var PORT = process.env.OPENSHIFT_INTERNAL_PORT || process.env.OPENSHIFT_NODEJS_PORT  || 8080;
+var IPADDRESS = process.env.OPENSHIFT_INTERNAL_IP || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+  
+  var io = require('socket.io').listen(8000);
 
    var players = {}, count = 0;
 
